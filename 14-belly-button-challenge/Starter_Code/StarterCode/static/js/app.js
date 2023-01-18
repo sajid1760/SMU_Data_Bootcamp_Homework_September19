86d7aa34a53f3ed69a6bb7b88892e134b0a1f70d
sajid1760 <61717892+sajid1760@users.noreply.github.com>
2023-01-18 08:51:51 -0600

    14-belly-button-challenge

--- 14-belly-button-challenge/Starter_Code/StarterCode/static/js/app.js
+++ 14-belly-button-challenge/Starter_Code/StarterCode/static/js/app.js
@@ -1,19 +1,18 @@
-//console.log(data);
-
-let smp_id = 940;
-
 
 
 url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
 $(window).ready(function() {
     let data = d3.json(url);
-    selectSubjectID();
-    doStuffWithData(smp_id);
+        d3.json(url).then(function(data) {
+        console.log(data);
+        selectSubjectID(data);
+        doStuffWithData(940, data);
+    });
 });
 
 
-function selectSubjectID() {
+function selectSubjectID(data) {
 
     let subject_ids = data.names;
 
@@ -28,13 +27,13 @@ function selectSubjectID() {
     d3.select("#selDataset").on("change", function() {
         let smp_id = d3.select("#selDataset").node().value;  
         console.log(smp_id);
-        doStuffWithData(smp_id);
+        doStuffWithData(smp_id, data);
        })
 
     
 }
 
-function doStuffWithData(smp_id) {
+function doStuffWithData(smp_id, data) {
 
 // transform data from JSON  into main components
 
